68d36037669d37d78e54ab0867d8a78dfaa7bf49
CaputoFederico <federicogcaputo@hotmail.com>
2023-08-07 17:18:34 -0300

    Avance desconocido con fallas en el html

---
+++ series/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from series.views import (
+    listar_series
+)
+
+
+urlpatterns = [
+    path('Series/', listar_series, name="lista_series"),
+    
+]  
--- series/views.py
+++ series/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.db.models import *
 
-# Create your views here.
+from series.models import Star_Trek, Star_Wars, Otras_Series
+
+
+def listar_series(request):
+    contexto = {
+        "Series" : Star_Trek.objects.all()
+    }
+    http_response = render(
+    request=request,
+    template_name= "series/star_trek.html",
+    context=contexto,
+    )
+    return http_response 

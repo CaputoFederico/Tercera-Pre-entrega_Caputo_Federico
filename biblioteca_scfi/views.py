09746ee125ae71b4818247bdda27d7e87f3b675f
CaputoFederico <federicogcaputo@hotmail.com>
2023-08-07 14:23:10 -0300

    Agregada primera app -series-

--- biblioteca_scfi/views.py
+++ biblioteca_scfi/views.py
@@ -1,1 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def inicio (request):
+    contexto= {}
+    http_response = render(
+        request=request,
+        template_name="inicio.html",
+        context=contexto,
+    )
+    return http_response
---
+++ series/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Star_Trek(models.Model):
+
+    serie=models.CharField(max_length=40)
+    temporadas= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.serie}, N° de temporadas: {self.temporadas}"
+
+
+class Star_Wars(models.Model):
+
+    serie=models.CharField(max_length=40)
+    temporadas= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.serie}, N° de temporadas: {self.temporadas}"
+
+
+class Otras_Series(models.Model):
+
+    serie=models.CharField(max_length=40)
+    temporadas= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.serie}, N° de temporadas: {self.temporadas}"
